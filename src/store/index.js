1eea327b7f246563067797c7107a59825354e4e4
domjan <domjan.daniel@gmail.com>
2021-07-02 17:31:59 +0200

    Fixed config mapping -REFACTOR LATER!!!!!

--- src/store/index.js
+++ src/store/index.js
@@ -193,7 +193,26 @@ export default new Vuex.Store({
       return state.editorConfig.settings.elements;
     },
     getConfigObject: state => {
-      return state.editorConfig;
+      //Deep copy
+      let x = JSON.parse(JSON.stringify(state.editorConfig));
+
+      let currentElems = x.settings.elements;
+      let keepElementsTab = false;
+
+      for (const key in currentElems) {
+        let keep = false;
+        let obj = currentElems[key];
+        for (const elem in obj) {
+          keep = keep || obj[elem];
+        }
+
+        if (!keep) currentElems[key] = false;
+        keepElementsTab = keepElementsTab || keep;
+      }
+
+      x.settings.elements = keepElementsTab ? currentElems : false;
+
+      return x;
     },
   },
 });
