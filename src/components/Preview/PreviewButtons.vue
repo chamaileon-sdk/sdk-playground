3f60e2c6dd2a553cac0d831ac443b5554f8b96d7
domjan <domjan.daniel@gmail.com>
2021-07-12 12:29:11 +0200

    Added preview page

---
+++ src/components/Preview/PreviewButtons.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Preview Buttons</h1>
+    <v-btn @click="openPreview">test</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPreview() {
+      this.$store.state.sdk.previewEmail({
+        document: {}, // email JSON - required
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Preview.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <Menu :items="editors" />
+
+    <v-main class="pa-16">
+      <v-container class="py-8 px-16" fluid>
+        <v-row>
+          <v-col>
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Menu from '../components/Menu.vue';
+
+export default {
+  async mounted() {
+    await this.$store.dispatch('updateSDK');
+  },
+
+  components: {
+    Menu,
+  },
+  data() {
+    return {
+      editors: [
+        {
+          title: 'Header',
+          icon: 'cog-outline',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
