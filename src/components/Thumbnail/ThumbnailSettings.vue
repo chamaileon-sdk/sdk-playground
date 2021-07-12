7ee7369cb7435c5d1610e71ca44778c3793b8b89
domjan <domjan.daniel@gmail.com>
2021-07-12 10:08:08 +0200

    Added thumbnail settings page

---
+++ src/components/Thumbnail/ThumbnailSettings.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <h1>Thumbnail Settings</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatibus,
+      saepe maxime autem asperiores consequatur quod, eligendi consectetur
+      repellendus dolore recusandae nobis fugit optio tempore deleniti.
+      Consectetur similique adipisci porro!
+    </p>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Thumbnail.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <Menu :items="editors" />
+
+    <v-main class="pa-16">
+      <v-container class="py-8 px-16" fluid>
+        <v-row>
+          <v-col>
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Menu from '../components/Menu.vue';
+
+export default {
+  components: {
+    Menu,
+  },
+  data() {
+    return {
+      editors: [
+        {
+          title: 'Settings',
+          icon: 'cog-outline',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
