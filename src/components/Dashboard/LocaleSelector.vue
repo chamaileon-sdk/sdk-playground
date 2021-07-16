36a3b567c59808469b2929453d22e3194bb297c0
domjan <domjan.daniel@gmail.com>
2021-07-16 04:49:27 +0200

    Extracted components from SDK page

---
+++ src/components/Dashboard/LocaleSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card flat class="pa-4">
+    <v-select
+      hide-details="true"
+      v-model="locale"
+      :items="['en', 'hu']"
+      outlined
+      label="Language"
+    ></v-select>
+  </v-card>
+</template>
+
+<script>
+export default {
+  computed: {
+    locale: {
+      get() {
+        return this.$store.state.sdkConfig.locale;
+      },
+      set(value) {
+        this.$store.commit('updateSDKConfig', { locale: value });
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
