58927eae2ec09bcd1d8cefba7b89f76e967146e8
domjan <domjan.daniel@gmail.com>
2021-07-20 11:41:02 +0200

    Created idContainer

---
+++ src/utilities/UtilityIdContainer.js
@@ -0,0 +1,23 @@
+class idContainer {
+	constructor() {
+		this.ids = new Set();
+		this.defaultId = 0;
+	}
+
+	addId = (id) => {
+		if (this.ids.has(id)) throw "Id is already in the container";
+
+		this.ids.add(id);
+		this.defaultId++;
+	};
+
+	removeId = (id) => {
+		if (!this.ids.has(id)) throw "Id is not in the container";
+
+		this.ids.delete(id);
+	};
+
+	hasId = (id) => this.ids.has(id);
+
+	getNextId = () => this.defaultId;
+}
