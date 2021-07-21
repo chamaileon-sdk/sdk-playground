3aa7c91158ab044df330b54cf939fdff2bb6b661
domjan <domjan.daniel@gmail.com>
2021-07-21 15:54:22 +0200

    Added copy to clipboard

--- src/components/AppCodeEditor.vue
+++ src/components/AppCodeEditor.vue
@@ -6,6 +6,14 @@
 		dark
 		flat
 	>
+		<v-card class="copyCard rounded-pill" elevation="0" v-show="snackbar"
+			><v-card-text class="pa-2 px-4 success--text"
+				>Code Copied to Clipboard</v-card-text
+			></v-card
+		>
+		<v-btn icon large class="copyToClipboard" @click="copyToClipboard(code)"
+			><v-icon>mdi-clipboard-file-outline</v-icon></v-btn
+		>
 		<highlight-code
 			style="position: fixed"
 			class="pa-0"
@@ -17,13 +25,56 @@
 
 <script>
 export default {
+	data: () => ({
+		snackbar: false,
+	}),
+
 	props: {
 		code: String,
+	},
+
+	methods: {
+		copyToClipboard(str) {
+			console.log("copy");
+			const el = document.createElement("textarea");
+			el.value = str;
+			el.setAttribute("readonly", "");
+			el.style.position = "absolute";
+			el.style.left = "-9999px";
+			document.body.appendChild(el);
+			el.select();
+			document.execCommand("copy");
+			document.body.removeChild(el);
+
+			this.snackbar = true;
+
+			setTimeout(() => {
+				this.snackbar = false;
+			}, 800);
+		},
 	},
 };
 </script>
 
 <style>
+.copyToClipboard {
+	position: fixed !important;
+	margin: 40px;
+	right: 0;
+	top: 0;
+	z-index: 1;
+	opacity: 0.15;
+}
+
+.copyCard {
+	position: fixed;
+	top: 0;
+	right: 0;
+	z-index: 2;
+	margin-right: 90px;
+	margin-top: 40px;
+}
+
 .hljs {
 	padding: 40px !important;
 	display: block;
