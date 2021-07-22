534da1ccf405aa2b249b0f1a2e813f4d85f62224
domjan <domjan.daniel@gmail.com>
2021-07-22 12:27:36 +0200

    Code editor final position

--- src/components/AppRouterViewContainer.vue
+++ src/components/AppRouterViewContainer.vue
@@ -1,22 +1,22 @@
 <template>
 	<v-row class="pa-0 ma-0">
-		<v-col class="px-16 pa-0" cols="12">
+		<v-col class="px-16 pa-0" cols="8">
 			<div class="px-16">
 				<slot />
 			</div>
 		</v-col>
-		<!--<v-col class="ma-0 pa-0" cols="4">
-			<CodeEditor :code="code" />
-		</v-col>-->
+		<v-col class="ma-0 pa-0" cols="4">
+			<CodeEditor class="editorContainer" :code="code" />
+		</v-col>
 	</v-row>
 </template>
 
 <script>
-//import CodeEditor from "../components/AppCodeEditor.vue";
+import CodeEditor from "../components/AppCodeEditor.vue";
 
 export default {
 	components: {
-		//CodeEditor,
+		CodeEditor,
 	},
 	props: {
 		code: String,
@@ -24,4 +24,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.editorContainer {
+	position: fixed;
+	right: 0;
+	width: calc((100% - 256px) * 0.3333) !important;
+}
+</style>
