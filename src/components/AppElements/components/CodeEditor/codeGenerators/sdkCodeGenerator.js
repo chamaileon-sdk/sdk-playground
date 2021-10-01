ce8bbfa3ac3664d3b455a585689226f3aa434b88
Lali <micskeil@gmail.com>
2021-10-01 15:40:10 +0200

    change to spaces

--- src/components/AppElements/components/CodeEditor/codeGenerators/sdkCodeGenerator.js
+++ src/components/AppElements/components/CodeEditor/codeGenerators/sdkCodeGenerator.js
@@ -1,17 +1,17 @@
 export default function (sdkConfig) {
 	return `const chamaileonPlugins = await window.chamaileonSdk.init({
-	mode: "serverless",
-	accessToken: accessToken,
-	whitelabel: {
-		urls: {
-			createLogoJS: "${sdkConfig.urls.createLogoJS}",
-			splashScreen: "${sdkConfig.urls.splashScreen}",
-		},
-		colors: {
-			primary: "${sdkConfig.colors.primary}",
-			secondary: "${sdkConfig.colors.secondary}",
-		},
-		locale: '${sdkConfig.locale}',
-	}
-});`;
+        mode: "serverless",
+        accessToken: accessToken,
+        whitelabel: {
+            urls: {
+                createLogoJS: "${sdkConfig.urls.createLogoJS}",
+                splashScreen: "${sdkConfig.urls.splashScreen}",
+            },
+            colors: {
+                primary: "${sdkConfig.colors.primary}",
+                secondary: "${sdkConfig.colors.secondary}",
+            },
+            locale: '${sdkConfig.locale}',
+        }
+    });`;
 }
