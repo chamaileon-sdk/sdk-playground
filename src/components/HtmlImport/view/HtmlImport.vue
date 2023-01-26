e7dda60c14d1bb1bb1d44e7d6559d4b19ef09248
Ladislav Misurak <lacee677@gmail.com>
2023-01-26 12:19:07 +0100

    chore: lint-fixes

--- src/components/HtmlImport/view/HtmlImport.vue
+++ src/components/HtmlImport/view/HtmlImport.vue
@@ -1,12 +1,12 @@
 <template>
 	<div>
 		<SectionObserver>
-			<div class="section" id="home">
+			<div id="home" class="section">
 				<Description
 					:title="'Email HTML Import'"
-					:docUrl="'https://chamaileon.io/sdk/v2/docs/email-html-importer/'"
+					:doc-url="'https://chamaileon.io/sdk/v2/docs/email-html-importer/'"
 					:image="'HtmlImportIllustration.svg'"
-					:hideGetStarted="true"
+					:hide-get-started="true"
 				>
 					<p>
 						Our API allows you to send existing HTML email code to the backend, and convert it to a JSON object,
@@ -15,7 +15,7 @@
 				</Description>
 			</div>
 		</SectionObserver>
-		<Footer :previous="'Email HTML Generator'" :prevTo="'/htmlgenerator'" />
+		<Footer :previous="'Email HTML Generator'" :prev-to="'/htmlgenerator'" />
 	</div>
 </template>
 
@@ -25,14 +25,14 @@ import Footer from "../../ViewUtilities/components/Footer.vue";
 import Description from "../../ViewUtilities/components/ViewDescription.vue";
 
 export default {
-	mounted() {
-		this.$store.dispatch("fetchDummyHtml");
-	},
 
 	components: {
 		SectionObserver,
 		Description,
 		Footer,
+	},
+	mounted() {
+		this.$store.dispatch("fetchDummyHtml");
 	},
 };
 </script>
