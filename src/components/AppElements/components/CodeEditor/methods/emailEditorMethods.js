24d5d348c238c8f03411338c9e804f718e592020
Igusius <61908293+Igusius@users.noreply.github.com>
2026-01-28 19:44:59 +0300

    add ai method

--- src/components/AppElements/components/CodeEditor/methods/emailEditorMethods.js
+++ src/components/AppElements/components/CodeEditor/methods/emailEditorMethods.js
@@ -6,6 +6,8 @@ export default function (editorConfig) {
 editorInstance.methods.updateSettings(${settingsGenerator(editorConfig, 1)});
 
 editorInstance.methods.updateHooks({ hooks: emailEditorHooks, resetHooks: false }); // see 'hooks' tab
+
+editorInstance.methods.startAIModal(); // directly opens AI Assistant with whole email context 
 `;
 	return str;
 }
